3c2eaeb650012c9eb65320b7db8e3e2e62c27b72
lingyunzheng <81157722+lingyunzheng@users.noreply.github.com>
2025-12-31 12:36:38 +0800

    Update page.tsx

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 export default function PrivacyPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800">
